ef99dbd8472090ada2dc04284c08ef778d969031
jrohan1 <j.rohan1@nuigalway.ie>
2018-11-14 19:53:04 +0000

    question 3
    question 3 complete

--- question_3.pl
+++ question_3.pl
@@ -1,7 +1,4 @@
-head(Element, [Element | _]).
 
-tail(Element, [_ | Element]).
+contains1(H,[H|_]).
 
-contains1(Element, Lst) :- head(Element, Lst).
-
-contains2(Element, Lst) :- tail(Element, Lst).
+contains2(T,[_|T]).
